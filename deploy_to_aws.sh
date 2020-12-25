66a4baa377b52fb1f3cd5bd06fc56019f641aa0e
AndreiGS <andreibv96@yahoo.com>
2020-12-25 20:20:02 +0200

    Create deploy_to_aws.sh

---
+++ deploy_to_aws.sh
@@ -0,0 +1,10 @@
+echo 'Starting to Deploy...'
+ssh ec2-user@ip-172-31-45-201 " 
+        sudo docker image prune -f 
+        cd ProduseSiServicii
+        sudo docker-compose down
+        git fetch origin
+        git reset --hard origin/master  &&  echo 'You are doing well'
+        sudo docker-compose build && sudo docker-compose up -d
+"
+echo 'Deployment completed successfully'
