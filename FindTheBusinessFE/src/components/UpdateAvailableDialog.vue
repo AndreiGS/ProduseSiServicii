2c6a4e5450820bbe43946969bedb08fb0aa4e9eb
Andrei <andreibv96@yahoo.com>
2020-12-26 21:39:21 +0200

    first-step

---
+++ FindTheBusinessFE/src/components/UpdateAvailableDialog.vue
@@ -0,0 +1,37 @@
+<script>
+import UpdateMixin from '@/components/mixins/update.js'
+
+export default {
+  name: 'UpdateAvailableDialog',
+  mixins: [
+    UpdateMixin
+  ]
+}
+</script>
+
+<template>
+  <div v-if="updateExists" id="modal-sections" uk-modal>
+    <div class="uk-modal-dialog uk-margin-auto-vertical">
+        <div class="uk-modal-header">
+          <h2 class="uk-modal-title">Actualizare</h2>
+        </div>
+        <div class="uk-modal-body">
+          <p>Apasati butonul de mai jos pentru a descarca ultima versiune</p>
+        </div>
+        <div class="uk-modal-footer uk-text-right">
+          <div class="uk-hidden@s">
+            <button @click="refreshApp" class="uk-button uk-button-primary custom-dialog-button uk-button-small" type="button">Actualizare</button>
+          </div>
+          <div class="uk-visible@s">
+            <button @click="refreshApp" class="uk-button uk-button-primary custom-dialog-button" type="button">Actualizare</button>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.custom-dialog-button{
+	margin-right: 2px;
+}
+</style>
---
+++ FindTheBusinessFE/src/service-worker.js
@@ -0,0 +1,37 @@
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(
+  new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
+  workbox.strategies.cacheFirst({
+    cacheName: "googleapis",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30
+      })
+    ],
+    method: "GET",
+    cacheableResponse: { statuses: [0, 200] }
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp("https://produsesiservicii.ro/(.*)"),
+  workbox.strategies.networkFirst({
+    cacheName: "produsesiserviciiapis",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30,
+        maxAgeSeconds: 7 * 24 * 60 * 60
+      })
+    ],
+    method: "GET",
+    cacheableResponse: { statuses: [0, 299] }
+  })
+)
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting()
+  }
+})
